116726c40f725e98a9015066c98d043aae5b53aa
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2023-07-20 00:56:34 +0800

    Small bugs fixed

--- src/labw_utils/commonutils/stdlib_helper/enum_helper.py
+++ src/labw_utils/commonutils/stdlib_helper/enum_helper.py
@@ -1,9 +1,20 @@
+"""
+TODO docs
+
+.. versionadded:: 1.0.3
+"""
+
 import enum
 
 from labw_utils.typing_importer import Any
 
 
 class EnhancedEnum(enum.Enum):
+    """
+    TODO docs
+
+    .. versionadded:: 1.0.3
+    """
     @classmethod
     def from_name(cls, in_name: str):
         for choices in cls:
